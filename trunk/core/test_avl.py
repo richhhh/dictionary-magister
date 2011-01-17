05714ea6ed41f25cecdd8cdfaf5a6ba632f557ff
pastuszka.przemyslaw <pastuszka.przemyslaw@3567de3c-6d5c-4bd1-109e-00956a6356d9>
2011-01-17 14:52:15 +0000

    Few new thing like count, modify. Tree's still not balanced.

--- trunk/core/test_avl.py
+++ trunk/core/test_avl.py
@@ -2,10 +2,11 @@ import unittest
 import random
 from avl import AVL
 
+
 class AVLTester(unittest.TestCase):
     def setUp(self):
         self.avl = AVL()
-        #insert random permutation of [1, ..., 1000] list to avl tree
+        #insert random permutation of [1, ..., 1000) list to avl tree
         ls = list(range(1, 1000))
         random.shuffle(ls)
         for v, k in enumerate(ls):
@@ -20,15 +21,24 @@ class AVLTester(unittest.TestCase):
         all_values = {self.avl.get_value_by_position(x) for x in range(1, 1000)}
         self.assertSetEqual(all_values, test_set, "get_value_by_position fails")
         #tree iterator works fine
-        all_values = {x.value for x in self.avl}
+        all_values = {value for key, value in self.avl}
         self.assertSetEqual(all_values, test_set, "tree iterator fails")
 
     def test_special_cases(self):
         self.assertIsNone(self.avl.get_value_by_key(-7))
         self.assertIsNone(self.avl.get_value_by_key(6666))
         self.assertIsNone(self.avl.get_value_by_position(6666))
-        self.assertEqual(self.avl.get_value_by_position(-21), self.avl.get_value_by_position(1))
-
+        self.assertIsNone(self.avl.get_value_by_position(-21))
+        self.assertIsNone(self.avl.get_value_by_closest_match(1001))
+        self.assertEqual(self.avl.get_value_by_closest_match(-1), self.avl.get_value_by_key(1))
+        self.assertIsNone(self.avl.get_value_by_closest_match(1001))
+        self.assertEqual(self.avl.count(), 999)
+        self.assertEqual(self.avl.count_subset(24, 124), 100)
+        self.assertEqual(self.avl.count_subset(-10, 10), 9)
+        self.assertEqual(self.avl.count_subset(999, 1001), 1)
+        self.assertEqual(self.avl.count_subset(997, 1001), 3)
+        self.assertEqual(self.avl.count_subset(1001, 1002), 0)
+        self.assertIsNone(self.avl.get_closest_element_position(10001))
 
 class AVLTester2(unittest.TestCase):
     def setUp(self):
@@ -41,8 +51,17 @@ class AVLTester2(unittest.TestCase):
         self.avl.insert(99, 22)
         self.avl.insert(111, 78)
         self.avl.insert(121, 91)
-        self.assertListEqual([78, 91, 19], [x.value for x in self.avl.get_iterator_by_key(111)])
-        self.assertListEqual([78, 91, 19], [x.value for x in self.avl.get_iterator_by_position(4)])
+        self.assertListEqual([78, 91, 19], [value for key, value in self.avl.get_iterator_by_key(111)])
+        self.assertListEqual([78, 91, 19], [value for key, value in self.avl.get_iterator_by_position(4)])
+        self.assertEqual(self.avl.get_value_by_closest_match(71), 99)
+        self.assertEqual(self.avl.get_value_by_closest_match(95), 22)
+        self.assertEqual(self.avl.get_closest_element_position(21), 1)
+        self.assertEqual(self.avl.get_closest_element_position(98), 3)
+        self.avl.modify_by_key(111, 19)
+        self.avl.modify_by_key(200, 21)
+        self.assertEqual(self.avl.get_value_by_key(111), 19)
+        self.avl.clear()
+        self.assertIsNone(self.avl.get_value_by_closest_match(111))
 
 #suite1 = unittest.TestLoader().loadTestsFromTestCase(AVLTester)
 #suite2 = unittest.TestLoader().loadTestsFromTestCase(AVLTester2)
